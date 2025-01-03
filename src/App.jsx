430783a07aeb681bd8c76c7c78e7aa0e448deec5
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-01-03 15:07:48 +0600

    Done!

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+export const App = () => {
+  return (
+    <div>
+
+    {/* <div>
+      <div>
+        <img src="img.jpg" alt="icon" width="80%" height="auto" />
+      </div>
+      <div>
+        <h2>Name: Ugreen Headphone</h2>
+        <h3>Ratings: 8.2</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div> */}
+
+
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+
+    </div>
+  );
+};
+
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+      
+        <img src="img.jpg" alt="icon" width="80%" height="auto" />
+      </div>
+      <div>
+        <h2>Name: Ugreen Headphone</h2>
+        <h3>Ratings: 8.2</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  );
+};

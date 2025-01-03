8a4b7bbc8cd2a81def6852496ab7d45c4e571694
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-01-03 20:55:37 +0600

    ssss

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import NetflixSeries from "./assets/components/NetflixSeries";
 
 export const App = () => {
   return (
-    <div>
+    <>
       <NetflixSeries  />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
-    </div>
+    </>
   );
 };

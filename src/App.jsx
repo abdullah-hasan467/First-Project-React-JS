bd70c62693a8d9be2b61ee226f76bb63cced9d93
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-01-03 20:17:19 +0600

    hhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
+import NetflixSeries from "./assets/components/NetflixSeries";
+
+
+
 export const App = () => {
   return (
     <div>
-
-    {/* <div>
-      <div>
-        <img src="img.jpg" alt="icon" width="80%" height="auto" />
-      </div>
-      <div>
-        <h2>Name: Ugreen Headphone</h2>
-        <h3>Ratings: 8.2</h3>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      </div>
-    </div> */}
-
-
-<NetflixSeries/>
-<NetflixSeries/>
-<NetflixSeries/>
-<NetflixSeries/>
-
-    </div>
-  );
-};
-
-const NetflixSeries = () => {
-  return (
-    <div>
-      <div>
-      <p></p>
-      <h3></h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non debitis reprehenderit ea excepturi dolore minima autem deserunt similique sunt, eos odio provident dolores beatae quia! Architecto eum temporibus fugiat quidem!	</p>
-      <img src="" alt="" srcset="" />
-        <img src="img.jpg" alt="icon" width="80%" height="auto" />
-      </div>
-      <div>
-        <h2>Name: Ugreen Headphone</h2>
-        <h3>Ratings: 8.2</h3>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      </div>
+      <NetflixSeries  />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
     </div>
   );
 };

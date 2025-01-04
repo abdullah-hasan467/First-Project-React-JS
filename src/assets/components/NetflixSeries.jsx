02aff1a2478b97592cc7b6547f7e69fc17a75143
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-01-04 13:43:06 +0600

    pp

--- src/assets/components/NetflixSeries.jsx
+++ src/assets/components/NetflixSeries.jsx
@@ -2,37 +2,44 @@ import seriesData from "/src/api/seriesData.json";
 
 const NetflixSeries = () => {
   return (
-    <div>
-      {seriesData.map((series, index) =>
-        <div key={index}>
-          <img src={series.img_url} alt="{series.name}" width="80%" height="auto" />
-          <div>
-            <h2>
-              Name: {series.name}
-            </h2>
-            <h3>
-              Ratings: {series.rating}
-            </h3>
-            <p>
-              Summary: {series.description}
-            </p>
-            <h3>
-              Cast: {series.cast}
-            </h3>
-            <h3>
-              Genre: {series.genre}
-            </h3>
-            <a
-              href={series.watch_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button> Watch Now </button>
-            </a>
-          </div>
-        </div>
-      )}
-    </div>
+    <ul>
+      {seriesData.map((curElem) => {
+        return (
+          <li key={curElem.id}>
+            <img
+              src={curElem.img_url}
+              alt={curElem.name}
+              width="80%"
+              height="auto"
+            />
+            <div>
+              <h2>
+                Name: {curElem.name}
+              </h2>
+              <h3>
+                Ratings: {curElem.rating}
+              </h3>
+              <p>
+                Summary: {curElem.description}
+              </p>
+              <h3>
+                Cast: {curElem.cast}
+              </h3>
+              <h3>
+                Genre: {curElem.genre}
+              </h3>
+              <a
+                href={curElem.watch_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button>Watch Now</button>
+              </a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 

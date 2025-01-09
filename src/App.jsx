3254956fecbea50239d961dd1a0bd6a1daaee10c
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-01-10 01:06:00 +0600

    ppp

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,41 @@
 // import { EventProps } from "./assets/components/EventProps.Jsx";
 // import "./assets/components/Netflix.module.css";
 
-import { ToggleSwitch } from "./assets/components/Projects/ToggleSwtich/ToggleSwitch";
+import { ReactUseEffect } from "./assets/components/hooks/useEffect/Index";
+
+// import { ContactForm } from "./assets/components/hooks/useState/ContactFrom";
+// import { LoginForm } from "./assets/components/hooks/useState/LoginForm";
+
+// import { RegistrationFormReact } from "./assets/components/hooks/useState/RegistrationFormReact";
 
 
+
+
+
+
+
+// import { Todo } from "./assets/components/Projects/ToggleSwtich/Todo/Todo";
+// import { ToggleSwitch } from "./assets/components/Projects/ToggleSwtich/ToggleSwitch";
 // import { Sate } from "./assets/components/hooks/State";
 
 
 export const App = () => {
-  return <section className="container">
-      {/* <h1 className="Title"> The Best Series for You </h1>
-      <NetflixSeries  /> */}
-      {/* <EventHandling/> */}
-      {/* <EventProps/> */}
-      {/* <Sate/> */}
-      {/* <LifitngState /> */}
-      <ToggleSwitch />
-    </section>;
+  return (
+    
+      <section className="container">
+        {/* <h1 className="Title"> The Best Series for You </h1>
+        <NetflixSeries  /> */}
+        {/* <EventHandling/> */}
+        {/* <EventProps/> */}
+        {/* <Sate/> */}
+        {/* <LifitngState /> */}
+        {/* <ToggleSwitch /> */}
+        {/* <Todo/> */}
+        {/* <RegistrationFormReact /> */}
+        {/* <LoginForm /> */}
+        {/* <ContactForm /> */}
+<ReactUseEffect/>
+      </section>
+    
+  );
 };
